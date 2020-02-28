b9ca135b8526e6448883b2a236c523d31a7b460c
Wilson Steven Suarez Suarez <1010086335@academico.icesi.edu.co>
2020-02-28 15:55:16 -0500

    Set

---
+++ Code/SpaceInvaders/src/main.java
@@ -0,0 +1,33 @@
+import processing.core.PApplet;
+
+public class main extends PApplet {
+	
+	int pantalla=0;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+PApplet.main("main");
+	}
+
+	public void settings() {
+		size(500,500);
+	}
+	
+	public void setup() {
+	
+	}
+
+	public void draw() {
+	background(0);
+	}
+	
+	
+	public void pintarpantallas() {
+		switch(pantalla) {
+		case 1:
+			
+		}
+			
+	}
+	
+}
